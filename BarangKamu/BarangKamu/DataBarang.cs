885e2f08c6b29cd0fe441a65a6226d6119250c7e
medavidrahmadana <me.davidrahmadana@gmail.com>
2023-05-04 23:16:47 +0700

    Project Selesai
    saat proses coding lupa tidak di git, ini adalah final proses nya

---
+++ BarangKamu/BarangKamu/DataBarang.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BarangKamu
+{
+    class Barang
+    {
+        public string NamaBarang { get; set; }
+        public int JumlahBarang { get; set; }
+
+        public Barang(string namaBarang, int jumlahBarang)
+        {
+            NamaBarang = namaBarang;
+            JumlahBarang = jumlahBarang;
+        }
+
+        public override string ToString()
+        {
+            return $"{NamaBarang} | {JumlahBarang}";
+        }
+    }
+
+    class DataBarang
+    {
+        private List<Barang> daftarBarang;
+
+        public DataBarang()
+        {
+            daftarBarang = new List<Barang>();
+        }
+
+        public void LoadData()
+        {
+            // Membaca file informasi barang
+            string path = "informasi_barang.txt";
+            if (File.Exists(path))
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] data = line.Split('|');
+                        string namaBarang = data[0].Trim();
+                        int jumlahBarang = int.Parse(data[1].Trim());
+                        Barang barang = new Barang(namaBarang, jumlahBarang);
+                        daftarBarang.Add(barang);
+                    }
+                }
+            }
+        }
+
+        public void SaveData()
+        {
+            // Menuliskan data barang ke file
+            string path = "informasi_barang.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach (Barang barang in daftarBarang)
+                {
+                    writer.WriteLine($"{barang.NamaBarang} | {barang.JumlahBarang}");
+                }
+            }
+        }
+
+        public void TambahBarang()
+        {
+            // Menambah barang
+            Console.WriteLine("===== Tambah Barang =====");
+            Console.Write("Masukkan nama barang: ");
+            string namaBarang = Console.ReadLine();
+            Console.Write("Masukkan jumlah barang: ");
+            int jumlahBarang = int.Parse(Console.ReadLine());
+            Barang barangBaru = new Barang(namaBarang, jumlahBarang);
+            daftarBarang.Add(barangBaru);
+            Console.WriteLine();
+            Console.WriteLine("Note: Barang berhasil ditambahkan.");
+            SaveData(); // Menyimpan perubahan ke dalam file
+        }
+
+        public void TampilkanBarang()
+        {
+            // Menampilkan daftar barang
+            Console.WriteLine("===== Daftar Barang =====");
+            if (daftarBarang.Count == 0)
+            {
+                Console.WriteLine("Data barang masih kosong");
+            }
+            else
+            {
+                Console.WriteLine("Nama Barang | Jumlah Barang");
+                foreach (Barang barang in daftarBarang)
+                {
+                    Console.WriteLine(barang);
+                }
+            }
+        }
+        public void UbahBarang()
+        {
+            // Mengubah barang
+            Console.WriteLine("===== Ubah Barang =====");
+            Console.Write("Masukkan nama barang yang akan diubah: ");
+            string namaBarangLama = Console.ReadLine();
+            Barang barang = daftarBarang.Find(b => b.NamaBarang == namaBarangLama);
+            if (barang == null)
+            {
+                Console.WriteLine("Note: Barang tidak ditemukan.");
+            }
+            else
+            {
+                Console.Write("Masukkan nama barang baru: ");
+                string namaBarangBaru = Console.ReadLine();
+                Console.Write("Masukkan jumlah barang baru: ");
+                int jumlahBarang = int.Parse(Console.ReadLine());
+                barang.NamaBarang = namaBarangBaru;
+                barang.JumlahBarang = jumlahBarang;
+                Console.WriteLine();
+                Console.WriteLine("Note: Barang berhasil diubah.");
+                SaveData(); // Menyimpan perubahan ke dalam file
+            }
+        }
+
+        public void HapusBarang()
+        {
+            // Menghapus barang
+            Console.WriteLine("===== Hapus Barang =====");
+            Console.Write("Masukkan nama barang yang akan diubah: ");
+            string namaBarang = Console.ReadLine();
+            Barang barang = daftarBarang.Find(b => b.NamaBarang == namaBarang);
+            if (barang == null)
+            {
+                Console.WriteLine("Barang tidak ditemukan.");
+            }
+            else
+            {
+                daftarBarang.Remove(barang);
+                Console.WriteLine();
+                Console.WriteLine("Note: Barang berhasil dihapus.");
+                SaveData(); // Menyimpan perubahan ke dalam file
+            }
+        }
+        public void HapusSemuaBarang()
+        {
+            // Menghapus seluruh data dalam file
+            string path = "informasi_barang.txt";
+            File.WriteAllText(path, string.Empty);
+            daftarBarang.Clear();
+            Console.WriteLine("Seluruh data berhasil dihapus.");
+        }
+    }
+}
---
+++ BarangKamu/BarangKamu/Program.cs
@@ -0,0 +1,62 @@
+﻿using BarangKamu;
+using System;
+
+namespace BarangKamu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBarang dataBarang = new DataBarang();
+            dataBarang.LoadData();
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("====|     Barang Kamu    |====");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("==== Aplikasi Data Barang ====");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("Berikut Menu Yang Tersedia: ");
+                Console.WriteLine("1. Tampilkan Barang");
+                Console.WriteLine("2. Tambah Barang");
+                Console.WriteLine("3. Ubah Barang");
+                Console.WriteLine("4. Hapus Barang");
+                Console.WriteLine("5. Hapus Semua Barang");
+                Console.WriteLine("6. Keluar");
+                Console.Write("Pilih menu (1-6): ");
+                string pilihan = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (pilihan)
+                {
+                    case "1":
+                        dataBarang.TampilkanBarang();
+                        break;
+                    case "2":
+                        dataBarang.TambahBarang();
+                        break;
+                    case "3":
+                        dataBarang.UbahBarang();
+                        break;
+                    case "4":
+                        dataBarang.HapusBarang();
+                        break;
+                    case "5":
+                        dataBarang.HapusSemuaBarang();
+                        break;
+                    case "6":
+                        exit = true;
+                        dataBarang.SaveData();
+                        Console.WriteLine("Terima kasih telah menggunakan aplikasi ini.");
+                        break;
+                    default:
+                        Console.WriteLine("Pilihan tidak valid.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
